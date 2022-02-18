24a99745171d087962137d506b0038dcba5a25d9
Noel <noelalonso1986@gmail.com>
2022-02-18 13:43:22 +0000

    Elimina test

--- src/test/java/es/redmic/user/integration/translations/ExceptionsTranslationTest.java
+++ src/test/java/es/redmic/user/integration/translations/ExceptionsTranslationTest.java
@@ -9,9 +9,9 @@ package es.redmic.user.integration.translations;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -94,23 +94,6 @@ public class ExceptionsTranslationTest {
 			String[] fields = (String[]) e.getFieldErrors().toArray();
 
 			String mess = messageSource.getMessage(code, fields, new Locale("es", "ES"));
-
-			Assert.assertEquals(getMessage(code, fields, resourcePathSpanish), mess);
-		}
-	}
-
-	@Test
-	public void getRussianLanguageMessage_returnDefaultI18nMessage_WhenI18nFileNotExist() {
-
-		try {
-			repository.findByEmail("prueba@redmic.es");
-		} catch (DBNotFoundException e) {
-
-			String code = e.getCode().toString();
-
-			String[] fields = (String[]) e.getFieldErrors().toArray();
-
-			String mess = messageSource.getMessage(code, fields, new Locale("ru", "RU"));
 
 			Assert.assertEquals(getMessage(code, fields, resourcePathSpanish), mess);
 		}
