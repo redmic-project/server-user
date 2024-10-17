1acb15183b794ce334955e6daa1e8920d493de5d
Noel Alonso <naloexp@grafcan.com>
2024-10-17 09:19:04 +0100

    Adapta config por cambio de rol

--- src/main/java/es/redmic/user/config/Oauth2SecurityConfiguration.java
+++ src/main/java/es/redmic/user/config/Oauth2SecurityConfiguration.java
@@ -9,9 +9,9 @@ package es.redmic.user.config;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,7 +65,7 @@ public class Oauth2SecurityConfiguration {
 		@Override
 		public void configure(HttpSecurity http) throws Exception {
 			// @formatter:off
-			
+
 			http.cors().and().anonymous().and().authorizeRequests().antMatchers("/user/actuator/**").permitAll()
 					.antMatchers(HttpMethod.GET, "/user/profile/").permitAll()
 					.antMatchers(HttpMethod.GET, "/user/modules/openmodules/").permitAll()
@@ -76,7 +76,7 @@ public class Oauth2SecurityConfiguration {
 					.antMatchers(HttpMethod.POST, "/user/activateAccount/**/").permitAll();
 
 			http.authorizeRequests().antMatchers("/**/").access(
-					"#oauth2.hasScope('read') or #oauth2.hasScope('write') and hasAnyRole('ROLE_ADMINISTRATOR', 'ROLE_OAG', 'ROLE_COLLABORATOR', 'ROLE_USER')");
+					"#oauth2.hasScope('read') or #oauth2.hasScope('write') and hasAnyRole('ROLE_ADMINISTRATOR', 'ROLE_MANAGER', 'ROLE_COLLABORATOR', 'ROLE_USER')");
 		}
 	}
 }
